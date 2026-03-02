c2c880c8f4108b2b3d487434d377294988fd57be
mrlvvan <jcf7ptjhk4@privaterelay.appleid.com>
2026-03-03 02:21:28 +0700

    Revert "Lab-1"

--- Lab-1/src/App.test.js
+++
@@ -1,7 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders auth screen title', () => {
-  render(<App />);
-  expect(screen.getByText(/Лабораторная работа 1/i)).toBeInTheDocument();
-});
